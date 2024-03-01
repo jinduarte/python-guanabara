2a9262cd6dba00e748038ad2b123badbb79f81aa
Jean Duarte <jeanduarte@gmail.com>
2024-03-01 19:40:24 -0300

    challenges proposed in lesson 04

---
+++ video04-challenge-003.py
@@ -0,0 +1,8 @@
+# Create a Python script that reads two numbers and tries to show the sum between them
+
+number1 = input('Primeiro número ') # First number
+number2 = input('Segundo número ') # Second number
+
+result = int(number1) + int(number2)
+
+print ('A soma é', result) #The sum is 'result'
