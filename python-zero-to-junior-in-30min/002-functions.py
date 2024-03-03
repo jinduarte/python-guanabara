ecf5020e517f37537eddcf661cb63e6b3c81314b
Jean Duarte <jeanduarte@gmail.com>
2024-03-02 22:56:09 -0300

    some more Python exercises

---
+++ python-zero-to-junior-in-30min/002-functions.py
@@ -0,0 +1,11 @@
+# Create a simple function example with Python
+
+def my_function(value1, value2):
+    return value1 + value2
+
+value1 = int(input('Value 1: '))
+value2 = int(input('Value 2: '))
+
+answer = my_function(value1,value2)
+
+print(value1, '+', value2, "=", answer)
